9c2d265568843978bca26044880ba5c592e93c7e
Yogesh Yadav <yogesh7132@gmail.com>
2020-06-26 09:11:16 +0530

    Added user inputs & wiki search feature

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -1,9 +1,12 @@
 import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
 
 engine = pyttsx3.init("sapi5")
-voices = engine.getProperty("voices")
-# print(voices[1].id)
-engine.setProperty("voices", voices[0].id)
+voice = engine.getProperty("voices")
+# print(voices[1].id
+engine.setProperty("voices", voice[0].id)
 
 
 def speak(audio):
@@ -11,5 +14,51 @@ def speak(audio):
     engine.runAndWait()
 
 
+def greet():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour <= 12:
+        speak("Good Morning!")
+    elif hour >= 12 and hour <= 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+    speak("I am scoopy sir, Please tell me how can i help you")
+
+
+def input():
+    """
+    This function takes microphone input from the user and return string output
+    """
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening ...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing ...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print(e)
+
+        print("Say that again please ...")
+        return "None"
+
+    return query
+
 if __name__ == "__main__":
-    speak("testing voice")
+    greet()
+    # input()
+
+    while True:
+        query = input().lower()
+
+        if 'wikipedia' in query:
+            speak("Searching Wikipedia")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query,sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
